c0444ec838a42ef79d967ccdd756b4578d7b4460
Max Wolter <awfm9@protonmail.com>
2022-10-07 21:08:45 -0500

    Fix Autohedge rehedging

--- position/autohedge.go
+++ position/autohedge.go
@@ -23,19 +23,20 @@ func NewAutohedge(input float64, price float64, ratio float64, swap float64) *Au
 func (a *Autohedge) Value(price float64) float64 {
 
 	volatile := math.Sqrt(a.liquidity / price)
+	stable := a.liquidity / volatile
 	switch {
 
 	case volatile < a.debt*(1-a.ratio):
 
 		delta := a.debt - volatile
 		amountStable := delta * price
-		a.liquidity -= (amountStable * delta)
+		a.liquidity = (volatile - delta) * (stable - amountStable)
 		a.debt -= (2 * delta)
 
 	case volatile > a.debt*(1+a.ratio):
 		delta := volatile - a.debt
 		amountStable := delta * price
-		a.liquidity += (amountStable * delta)
+		a.liquidity = (volatile + delta) * (stable + amountStable)
 		a.debt += (2 * delta)
 	}
 
